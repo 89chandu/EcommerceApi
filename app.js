59d5c5173d2b7b123dcb7c4c2959ee571545dbff
Chandu Bopche <Chandubopche321@gmail.com>
2023-08-15 19:07:39 +0530

    done now

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const bodyParser = require('body-parser')
 const StoreRoute = require("./Routes/StoreItems");
 const CartRoute = require('./Routes/CartItems');
-// const products_routes = require("./Routes/Product");
 const UserRoute = require('./Routes/Users');
 const connectDB = require("./DB/connect");
 const cors = require('cors');
@@ -13,18 +12,24 @@ const app = Express();
  
 app.use(cors());
 app.use(bodyParser.json());
-app.use("/store", StoreRoute);
+
 app.use('/cart', CartRoute);
-// app.use('/products', ProductRoute);
 app.use('/users', UserRoute)
-// app.use("/api/products", products_routes);
+
+app.get("/", (req, res) => {
+	res.send("Hi, I am live ");
+  });
+
+  app.use('/store', StoreRoute);
+
+
 
 
 
 const PORT = process.env.PORT||3005
 async function serverStart() {
 	try {
-		await connectDB(process.env.MONGODB_URL);
+		await connectDB();
 		app.listen(PORT, () => {
 			console.log(`Yes I am connected ${PORT}`);
 
