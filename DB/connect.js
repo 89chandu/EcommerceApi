f9ec78be8aa45921f228d627e4d74cbe382a2a81
Chandu Bopche <82381986+89chandu@users.noreply.github.com>
2023-08-20 09:31:32 +0530

    Update connect.js

--- DB/connect.js
+++ DB/connect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
 	try {
 		const conn = await mongoose.connect(
-			`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`,
+			`mongodb+srv://chandubopche321:k7bdZLhXh8ii5bpy@cluster0.mbmsuxg.mongodb.net/EcomReact?retryWrites=true&w=majority`,
 			{
 				useNewUrlParser: true, // It is used to parse and interpret the MongoDB connection string in a way that is compatible with the MongoDB Node.js driver's updated URL parser.
 			}
